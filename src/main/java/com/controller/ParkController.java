4de5aa8fbdd540b3d06d87eec8d8288911b0613a
Quenuo <angelsanchezvallejomoreno@gmail.com>
2024-12-16 19:54:24 +0100

    Agregado las imagenes

---
+++ src/main/java/com/controller/ParkController.java
@@ -0,0 +1,31 @@
+package com.controller;
+
+//@RestController
+//@RequestMapping("/park")
+public class ParkController {
+//    private final ParkService parkService;
+//    @Autowired
+//    public ParkController(ParkService parkService){
+//        this.parkService=parkService;
+//    }
+//
+//    @GetMapping("/enclosures")
+//    public List<Enclousure> allEnclosures(){
+//        return parkService.getAllEnclousures();
+//    }
+//
+//    @GetMapping("/emergencies")
+//    public List<Emergency> allEmergencies(){
+//        return parkService.getAllEmergency();
+//    }
+//
+//    @GetMapping("/dinosaurs")
+//    public List<Dinosaur> alldinosaurs(){
+//        return parkService.getAllDinosaur();
+//    }
+
+
+
+
+
+}
