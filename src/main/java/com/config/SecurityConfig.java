666b1e1d17363285ec411aab966e893238315956
Quenuo <angelsanchezvallejomoreno@gmail.com>
2025-01-13 17:26:58 +0100

    Refactor: allower render host to cors

--- src/main/java/com/config/SecurityConfig.java
+++ src/main/java/com/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://jurasickparkmanagement.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
         configuration.setExposedHeaders(List.of("Authorization"));
